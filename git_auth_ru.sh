a58b6e85666f2c9162337171a4bf4a67955e6c2b
Rikke Uldbæk <75247443+rikkeuldbaek@users.noreply.github.com>
2023-05-23 10:33:06 +0200

    Delete git_auth_ru.sh

--- git_auth_ru.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-git config --global user.email "rikke_uldbaek@hotmail.com"
-git config --global user.name "rikkeuldbaek"
-
-#install requirements
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-python3 -m spacy download en_core_web_sm
